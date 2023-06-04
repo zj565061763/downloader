a00fb2fb1e8a5ec12cab08e219933eebe865aaa1
zj565061763 <565061763@qq.com>
2023-06-04 22:50:09 +0800

    1.0.0-alpha01

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "downloader"
-val libVersionName = "1.0.0-beta01"
+val libVersionName = "1.0.0-alpha01"
 
 android {
     namespace = "com.sd.lib.downloader"
