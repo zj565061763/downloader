9878573d7dd419564b8f8f3e8977fd95a9aa454f
zj565061763 <565061763@qq.com>
2023-06-04 17:12:40 +0800

    SimpleDownloadExecutor

---
+++ lib/src/main/java/com/sd/lib/downloader/executor/SimpleDownloadExecutor.kt
@@ -0,0 +1,162 @@
+package com.sd.lib.downloader.executor
+
+import com.github.kevinsawicki.http.HttpRequest
+import com.sd.lib.downloader.DownloadRequest
+import com.sd.lib.downloader.exception.DownloadHttpExceptionResponseCode
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.InputStream
+import java.io.RandomAccessFile
+import java.net.HttpURLConnection
+import java.util.Collections
+import kotlin.coroutines.cancellation.CancellationException
+
+class SimpleDownloadExecutor @JvmOverloads constructor(
+    limitedParallelism: Int = 3,
+    preferBreakpoint: Boolean = false,
+) : IDownloadExecutor {
+
+    private val _preferBreakpoint = preferBreakpoint
+    private val _taskHolder: MutableMap<String, Job> = Collections.synchronizedMap(hashMapOf())
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val _scope by lazy {
+        CoroutineScope(Dispatchers.IO.limitedParallelism(limitedParallelism))
+    }
+
+    private fun newHttpRequest(downloadRequest: DownloadRequest): HttpRequest {
+        return HttpRequest.get(downloadRequest.url)
+            .connectTimeout(15 * 1000)
+            .readTimeout(15 * 1000)
+            .trustAllHosts()
+            .trustAllCerts()
+    }
+
+    override fun submit(request: DownloadRequest, file: File, updater: IDownloadExecutor.Updater) {
+        val url = request.url
+        _scope.launch(
+            context = CoroutineExceptionHandler { _, _ -> },
+            start = CoroutineStart.LAZY,
+        ) {
+            val length = file.length()
+            val breakpoint = request.preferBreakpoint ?: _preferBreakpoint && length > 0
+
+            if (breakpoint) {
+                val httpRequest = newHttpRequest(request)
+                val code = httpRequest.run {
+                    this.header("Range", "bytes=$length-")
+                    this.code()
+                }
+                if (code == HttpURLConnection.HTTP_PARTIAL) {
+                    downloadBreakpoint(
+                        httpRequest = httpRequest,
+                        file = file,
+                        updater = updater,
+                    )
+                    return@launch
+                }
+            }
+
+            val httpRequest = newHttpRequest(request)
+            val code = httpRequest.code()
+            if (code == HttpURLConnection.HTTP_OK) {
+                downloadNormal(
+                    httpRequest = httpRequest,
+                    file = file,
+                    updater = updater,
+                )
+            } else {
+                throw DownloadHttpExceptionResponseCode(code)
+            }
+        }.also { job ->
+            _taskHolder[url] = job
+            job.invokeOnCompletion { t ->
+                _taskHolder.remove(url)
+                when (t) {
+                    null -> updater.notifySuccess()
+                    is CancellationException -> updater.notifyCancel()
+                    else -> updater.notifyError(t)
+                }
+            }
+            job.start()
+        }
+    }
+
+    override fun cancel(url: String?): Boolean {
+        if (url.isNullOrEmpty()) return false
+        val job = _taskHolder[url] ?: return false
+        job.cancel()
+        return true
+    }
+
+    private fun downloadNormal(
+        httpRequest: HttpRequest,
+        file: File,
+        updater: IDownloadExecutor.Updater,
+    ) {
+        val total = httpRequest.contentLength().toLong()
+        httpRequest.stream().use { input ->
+            file.outputStream().use { output ->
+                input.copyToOutput(
+                    output = { buffer, offset, length ->
+                        output.write(buffer, offset, length)
+                    },
+                    callback = { count ->
+                        updater.notifyProgress(total, count)
+                        false
+                    },
+                )
+            }
+        }
+    }
+
+    private fun downloadBreakpoint(
+        httpRequest: HttpRequest,
+        file: File,
+        updater: IDownloadExecutor.Updater,
+    ) {
+        val length = file.length()
+        val randomAccessFile = RandomAccessFile(file, "rwd").apply {
+            this.seek(length)
+        }
+
+        val total = httpRequest.contentLength() + length
+        httpRequest.stream().use { input ->
+            randomAccessFile.use { output ->
+                input.copyToOutput(
+                    output = { buffer, offset, length ->
+                        output.write(buffer, offset, length)
+                    },
+                    callback = { count ->
+                        updater.notifyProgress(total, count + length)
+                        false
+                    },
+                )
+            }
+        }
+    }
+}
+
+private fun InputStream.copyToOutput(
+    output: (buffer: ByteArray, offset: Int, length: Int) -> Unit,
+    callback: ((count: Long) -> Boolean)? = null,
+): Long {
+    var bytesCopied: Long = 0
+    val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+    var bytes = read(buffer)
+    while (bytes >= 0) {
+        output(buffer, 0, bytes)
+
+        bytesCopied += bytes
+        if (callback?.invoke(bytesCopied) == true) break
+
+        bytes = read(buffer)
+    }
+    return bytesCopied
+}
