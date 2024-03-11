b12a009fecf163e8d5297eeaabb5de51bbcf72c3
zjun <565061763@qq.com>
2024-03-11 15:09:40 +0800

    demo

--- app/src/main/java/com/sd/demo/downloader/MainActivity.kt
+++ app/src/main/java/com/sd/demo/downloader/MainActivity.kt
@@ -1,118 +1,23 @@
 package com.sd.demo.downloader
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.activity.ComponentActivity
-import com.sd.lib.downloader.DownloadRequest
-import com.sd.lib.downloader.FDownloader
-import com.sd.lib.downloader.IDownloadInfo
-import com.sd.lib.downloader.IDownloader
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-
-private const val URL = "https://dldir1.qq.com/weixin/Windows/WeChatSetup.exe"
+import com.sd.demo.downloader.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
-    private val _scope = MainScope()
-    private var _awaitJob: Job? = null
+    private val _binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<View>(R.id.btn_download).setOnClickListener {
-            logMsg { "click download" }
-            download()
+        setContentView(_binding.root)
+        _binding.btnSampleDownload.setOnClickListener {
+            startActivity(Intent(this, SampleDownload::class.java))
         }
-
-        findViewById<View>(R.id.btn_await_download).setOnClickListener {
-            logMsg { "click await download" }
-            awaitDownload()
+        _binding.btnSampleAwaitDownload.setOnClickListener {
+            startActivity(Intent(this, SampleAwaitDownload::class.java))
         }
-
-        findViewById<View>(R.id.btn_cancel).setOnClickListener {
-            logMsg { "click cancel" }
-            cancelDownload()
-        }
-    }
-
-    private fun getDownloadRequest(): DownloadRequest {
-        return DownloadRequest.Builder()
-            // true-优先断点下载；false-不使用断点下载；null-跟随初始化配置
-            .setPreferBreakpoint(true)
-            // 下载地址
-            .build(URL)
-    }
-
-    private fun download() {
-        _awaitJob?.cancel()
-
-        // 添加下载回调
-        FDownloader.addCallback(_downloadCallback)
-
-        // 添加下载任务
-        FDownloader.addTask(getDownloadRequest())
-    }
-
-    private fun awaitDownload() {
-        // 移除下载回调
-        FDownloader.removeCallback(_downloadCallback)
-
-        _awaitJob?.cancel()
-        _scope.launch {
-            FDownloader.awaitTask(
-                request = getDownloadRequest(),
-                callback = object : IDownloader.Callback {
-                    override fun onDownloadInfo(info: IDownloadInfo) {
-                        logMsg { "await onDownloadInfo:$info" }
-                    }
-                }
-            ).let { result ->
-                result.onSuccess {
-                    logMsg { "await success $it" }
-                }
-                result.onFailure {
-                    logMsg { "await failure $it ${it.javaClass.name}" }
-                }
-            }
-        }.also { _awaitJob = it }
-    }
-
-    private fun cancelDownload() {
-        // 取消下载任务
-        FDownloader.cancelTask(URL)
-    }
-
-    /**
-     * 下载回调
-     */
-    private val _downloadCallback: IDownloader.Callback = object : IDownloader.Callback {
-        override fun onDownloadInfo(info: IDownloadInfo) {
-            when (info) {
-                is IDownloadInfo.Initialized -> logMsg { "onInitialized" }
-                is IDownloadInfo.Progress -> logMsg { "onProgress ${info.progress}" }
-                is IDownloadInfo.Success -> logMsg { "onSuccess file:${info.file.absolutePath}" }
-                is IDownloadInfo.Error -> logMsg { "onError ${info.exception.javaClass.name}" }
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _scope.cancel()
-        cancelDownload()
-
-        // 移除下载回调
-        FDownloader.removeCallback(_downloadCallback)
-
-        // 删除所有临时文件（下载中的临时文件不会被删除）
-        FDownloader.deleteTempFile()
-
-        // 删除下载文件（临时文件不会被删除）
-        FDownloader.deleteDownloadFile(null)
     }
 }
 
