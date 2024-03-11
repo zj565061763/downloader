2f2783ca20a92a0e284abd7ac779a3c27291537e
zjun <565061763@qq.com>
2024-03-11 14:59:06 +0800

    SampleDownload

---
+++ app/src/main/java/com/sd/demo/downloader/SampleDownload.kt
@@ -0,0 +1,75 @@
+package com.sd.demo.downloader
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.sd.demo.downloader.databinding.SampleDownloadBinding
+import com.sd.lib.downloader.DownloadRequest
+import com.sd.lib.downloader.FDownloader
+import com.sd.lib.downloader.IDownloadInfo
+import com.sd.lib.downloader.IDownloader
+
+class SampleDownload : ComponentActivity() {
+    private val _binding by lazy { SampleDownloadBinding.inflate(layoutInflater) }
+    private val url = "https://dldir1.qq.com/weixin/Windows/WeChatSetup.exe"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(_binding.root)
+        _binding.btnDownload.setOnClickListener { startDownload() }
+        _binding.btnCancel.setOnClickListener { cancelDownload() }
+    }
+
+    /**
+     * 开始下载
+     */
+    private fun startDownload() {
+        // 添加下载回调
+        FDownloader.addCallback(_downloadCallback)
+
+        // 构建下载请求
+        val request = DownloadRequest.Builder()
+            // true-优先断点下载；false-不使用断点下载；null-跟随初始化配置
+            .setPreferBreakpoint(true)
+            // 下载地址
+            .build(url)
+
+        // 添加下载任务
+        FDownloader.addTask(request)
+    }
+
+    /**
+     * 取消下载
+     */
+    private fun cancelDownload() {
+        // 取消下载任务
+        FDownloader.cancelTask(url)
+
+        // 移除下载回调
+        FDownloader.removeCallback(_downloadCallback)
+    }
+
+    /**
+     * 下载回调
+     */
+    private val _downloadCallback = object : IDownloader.Callback {
+        override fun onDownloadInfo(info: IDownloadInfo) {
+            when (info) {
+                is IDownloadInfo.Initialized -> logMsg { "callback Initialized" }
+                is IDownloadInfo.Progress -> logMsg { "callback Progress ${info.progress}" }
+                is IDownloadInfo.Success -> logMsg { "callback Success file:${info.file.absolutePath}" }
+                is IDownloadInfo.Error -> logMsg { "callback Error ${info.exception.javaClass.name}" }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancelDownload()
+
+        // 删除所有临时文件（下载中的临时文件不会被删除）
+        FDownloader.deleteTempFile()
+
+        // 删除下载文件（临时文件不会被删除）
+        FDownloader.deleteDownloadFile(null)
+    }
+}
