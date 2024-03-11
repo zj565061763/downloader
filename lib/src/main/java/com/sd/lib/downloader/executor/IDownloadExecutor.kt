1f6a51d8d9d8d67f9b276d473df99ac8253321ec
zjun <565061763@qq.com>
2024-03-11 12:17:59 +0800

    format

--- lib/src/main/java/com/sd/lib/downloader/executor/IDownloadExecutor.kt
+++ lib/src/main/java/com/sd/lib/downloader/executor/IDownloadExecutor.kt
@@ -7,16 +7,18 @@ import java.io.File
 interface IDownloadExecutor {
     /**
      * 提交下载任务
-     *
      * @param request 下载请求
      * @param file    要保存的下载文件
      * @param updater 下载信息更新对象
      */
-    fun submit(request: DownloadRequest, file: File, updater: Updater)
+    fun submit(
+        request: DownloadRequest,
+        file: File,
+        updater: Updater,
+    )
 
     /**
      * 取消[url]下载任务
-     *
      * @return true-任务取消
      */
     fun cancel(url: String?): Boolean
@@ -24,7 +26,6 @@ interface IDownloadExecutor {
     interface Updater {
         /**
          * 通知下载进度
-         *
          * @param total   总数量
          * @param current 已传输数量
          */
