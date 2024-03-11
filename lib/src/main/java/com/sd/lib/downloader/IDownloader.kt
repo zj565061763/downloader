a099de90c442ffcf89ef3f7e605568b43f6a731d
zjun <565061763@qq.com>
2024-03-11 12:22:29 +0800

    comment

--- lib/src/main/java/com/sd/lib/downloader/IDownloader.kt
+++ lib/src/main/java/com/sd/lib/downloader/IDownloader.kt
@@ -14,20 +14,19 @@ interface IDownloader {
     fun removeCallback(callback: Callback)
 
     /**
-     * 返回[url]对应的文件
-     *
-     * @return null-文件不存在；不为null-下载文件存在
+     * 获取[url]对应的下载文件
+     * @return null-不存在；不为null-存在
      */
     fun getDownloadFile(url: String?): File?
 
     /**
-     * 删除下载文件（临时文件不会被删除）
-     * @param ext 文件扩展名(例如mp3)；null-删除所有下载文件
+     * 删除下载文件(临时文件不会被删除)
+     * @param ext 文件扩展名(例如mp3)，null-删除所有下载文件
      */
     fun deleteDownloadFile(ext: String?)
 
     /**
-     * 删除所有临时文件（下载中的临时文件不会被删除）
+     * 删除所有临时文件(下载中的临时文件不会被删除)
      */
     fun deleteTempFile()
 
@@ -38,21 +37,18 @@ interface IDownloader {
 
     /**
      * 添加下载任务
-     *
      * @return true-任务添加成功或者已经添加
      */
     fun addTask(url: String?): Boolean
 
     /**
      * 添加下载任务
-     *
      * @return true-任务添加成功或者已经添加
      */
     fun addTask(request: DownloadRequest): Boolean
 
     /**
      * 取消下载任务
-     *
      * @return true-任务被取消
      */
     fun cancelTask(url: String?): Boolean
