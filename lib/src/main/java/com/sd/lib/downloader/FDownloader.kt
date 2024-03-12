f5abee7c4586494f2683baa9f3cec0ba52d48a0d
zjun <565061763@qq.com>
2024-03-12 12:47:37 +0800

    log

--- lib/src/main/java/com/sd/lib/downloader/FDownloader.kt
+++ lib/src/main/java/com/sd/lib/downloader/FDownloader.kt
@@ -156,7 +156,7 @@ object FDownloader : IDownloader {
             removeTask(task.url)
             val info = IDownloadInfo.Error(task.url, exception)
             notifyDownloadInfo(info) {
-                logMsg { "notify callback Error url:${task.url} exception:${exception.javaClass.simpleName}" }
+                logMsg { "notify callback Error url:${task.url} exception:${exception.javaClass.simpleName} $exception" }
             }
         }
     }
