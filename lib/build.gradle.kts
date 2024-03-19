223d99a380c444f64dea7ab7e46ac567883d3226
zjun <565061763@qq.com>
2024-03-19 13:47:56 +0800

    1.0.0-alpha10

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "downloader"
-val libVersionName = "1.0.0-alpha09"
+val libVersionName = "1.0.0-alpha10"
 
 android {
     namespace = "com.sd.lib.downloader"
