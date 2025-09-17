4510b5855ea4a8e5cc92c23e4e7dbf766d691880
zjun <565061763@qq.com>
2025-09-17 22:36:37 +0800

    Sample

--- app/src/main/java/com/sd/demo/downloader/App.kt
+++ app/src/main/java/com/sd/demo/downloader/App.kt
@@ -8,10 +8,7 @@ class App : Application() {
     super.onCreate()
     DownloaderConfig.init(
       DownloaderConfig.Builder()
-
-        // 设置是否输出日志（tag：FDownloader），默认：false
         .setDebug(true)
-
         .build(this)
     )
   }
