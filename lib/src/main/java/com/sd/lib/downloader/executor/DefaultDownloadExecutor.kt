e585e9c7949950a9d0daa7999076f50427b86865
zjun <565061763@qq.com>
2025-09-18 11:33:55 +0800

    本地文件大小大于0才考虑断点下载逻辑

--- lib/src/main/java/com/sd/lib/downloader/executor/DefaultDownloadExecutor.kt
+++ lib/src/main/java/com/sd/lib/downloader/executor/DefaultDownloadExecutor.kt
@@ -70,7 +70,7 @@ class DefaultDownloadExecutor @JvmOverloads constructor(
     updater: DownloadExecutor.Updater,
   ) {
     val length = file.length()
-    val breakpoint = request.preferBreakpoint ?: (_preferBreakpoint && length > 0)
+    val breakpoint = (request.preferBreakpoint ?: _preferBreakpoint) && length > 0
 
     if (breakpoint) {
       val httpRequest = newHttpRequest(request)
