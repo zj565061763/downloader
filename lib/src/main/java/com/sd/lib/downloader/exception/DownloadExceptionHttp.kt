4d638c6a59bba57af60351b1ba20191b46f80181
zjun <565061763@qq.com>
2025-09-18 15:51:51 +0800

    异常描述

--- lib/src/main/java/com/sd/lib/downloader/exception/DownloadExceptionHttp.kt
+++ lib/src/main/java/com/sd/lib/downloader/exception/DownloadExceptionHttp.kt
@@ -2,11 +2,19 @@ package com.sd.lib.downloader.exception
 
 import android.content.Context
 import com.sd.lib.downloader.R
+import java.net.SocketTimeoutException
 
 open class DownloadExceptionHttp(
   message: String? = null,
   cause: Throwable? = null,
-) : DownloadException(message, cause)
+) : DownloadException(message, cause) {
+  override fun formatCause(context: Context): String {
+    return when (cause) {
+      is SocketTimeoutException -> context.getString(R.string.lib_downloader_ExceptionHttp_SocketTimeoutException)
+      else -> super.formatCause(context)
+    }
+  }
+}
 
 class DownloadExceptionHttpResponseCode(val code: Int) : DownloadExceptionHttp() {
   override fun formatMessage(context: Context): String {
