277883ee6117abffad3ebdf684b91dd2f3eeaa8f
zjun <565061763@qq.com>
2025-09-18 18:17:05 +0800

    异常描述

--- lib/src/main/java/com/sd/lib/downloader/exception/DownloadExceptionHttp.kt
+++ lib/src/main/java/com/sd/lib/downloader/exception/DownloadExceptionHttp.kt
@@ -3,6 +3,7 @@ package com.sd.lib.downloader.exception
 import android.content.Context
 import com.sd.lib.downloader.R
 import java.net.ConnectException
+import java.net.SocketException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 import javax.net.ssl.SSLHandshakeException
@@ -14,6 +15,7 @@ open class DownloadExceptionHttp(
   override fun formatMessage(context: Context): String? {
     return when (cause) {
       is ConnectException -> ""
+      is SocketException -> ""
       is SocketTimeoutException -> ""
       is UnknownHostException -> ""
       is SSLHandshakeException -> ""
@@ -24,6 +26,7 @@ open class DownloadExceptionHttp(
   override fun formatCause(context: Context): String {
     return when (cause) {
       is ConnectException -> context.getString(R.string.lib_downloader_ExceptionHttp_ConnectException)
+      is SocketException -> context.getString(R.string.lib_downloader_ExceptionHttp_SocketException)
       is SocketTimeoutException -> context.getString(R.string.lib_downloader_ExceptionHttp_SocketTimeoutException)
       is UnknownHostException -> context.getString(R.string.lib_downloader_ExceptionHttp_UnknownHostException)
       is SSLHandshakeException -> context.getString(R.string.lib_downloader_ExceptionHttp_SSLHandshakeException)
