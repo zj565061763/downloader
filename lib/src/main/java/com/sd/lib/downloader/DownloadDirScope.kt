88a5bb28bded79786a3feffbd883add9112e2a5c
zjun <565061763@qq.com>
2025-09-25 20:45:36 +0800

    deleteAllDownloadFileExcept

--- lib/src/main/java/com/sd/lib/downloader/DownloadDirScope.kt
+++ lib/src/main/java/com/sd/lib/downloader/DownloadDirScope.kt
@@ -1,11 +1,16 @@
 package com.sd.lib.downloader
 
+import java.io.File
+
 interface DownloadDirScope {
   /** 删除子目录下的所有临时文件（不含下载中的临时文件），并返回删除的个数 */
   fun deleteAllTempFile(): Int
 
   /** 删除子目录下的所有下载文件（不含临时文件），并返回删除的个数 */
   fun deleteAllDownloadFile(): Int
+
+  /** 不删除[url]对应的下载文件，其他逻辑同[deleteAllDownloadFile] */
+  fun deleteAllDownloadFileExcept(url: String): Int
 }
 
 internal class DownloadDirScopeImpl(
@@ -15,15 +20,30 @@ internal class DownloadDirScopeImpl(
   override fun deleteAllTempFile(): Int {
     return dir.tempFiles(dirname = dirname) { files ->
       var count = 0
-      files.forEach { if (it.delete()) count++ }
+      files.forEach { file ->
+        if (file.delete()) count++
+      }
       count
     }
   }
 
   override fun deleteAllDownloadFile(): Int {
+    return deleteAllDownloadFile { true }
+  }
+
+  override fun deleteAllDownloadFileExcept(url: String): Int {
+    val exceptFile = dir.fileForKey(dirname = dirname, key = url)
+    return deleteAllDownloadFile { it != exceptFile }
+  }
+
+  private fun deleteAllDownloadFile(block: (File) -> Boolean): Int {
     return dir.files(dirname = dirname) { files ->
       var count = 0
-      files.forEach { if (it.delete()) count++ }
+      files.forEach { file ->
+        if (block(file)) {
+          if (file.delete()) count++
+        }
+      }
       count
     }
   }
