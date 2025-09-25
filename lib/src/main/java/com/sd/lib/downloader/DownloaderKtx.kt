464977b85bc5f1a37a4fb0cd111b46f9cf3b229c
zjun <565061763@qq.com>
2025-09-25 14:14:49 +0800

    ktx

--- lib/src/main/java/com/sd/lib/downloader/DownloaderKtx.kt
+++ lib/src/main/java/com/sd/lib/downloader/DownloaderKtx.kt
@@ -1,6 +1,7 @@
 package com.sd.lib.downloader
 
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.buffer
@@ -8,22 +9,6 @@ import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.suspendCancellableCoroutine
 import java.io.File
 import kotlin.coroutines.resume
-
-fun Downloader.Companion.downloadInfoFlow(url: String? = null): Flow<DownloadInfo> {
-  return callbackFlow {
-    val callback = object : DownloadInfoCallback {
-      override fun onDownloadInfo(info: DownloadInfo) {
-        if (url == null || url == info.url) {
-          trySend(info)
-        }
-      }
-    }
-    FDownloader.registerCallback(callback)
-    awaitClose {
-      FDownloader.unregisterCallback(callback)
-    }
-  }.buffer(Channel.UNLIMITED)
-}
 
 /**
  * 添加下载任务，注意：如果当前协程被取消，不会取消下载任务
@@ -63,4 +48,36 @@ suspend fun Downloader.addTaskAwait(
       unregisterCallback(realCallback)
     }
   }
+}
+
+/** 监听所有下载信息 */
+fun Downloader.Companion.downloadInfoFlow(): Flow<DownloadInfo> {
+  return downloadInfoFlow { info ->
+    trySend(info)
+  }
+}
+
+/** 监听指定[url]的下载信息 */
+fun Downloader.Companion.downloadInfoFlowOf(url: String): Flow<DownloadInfo> {
+  return downloadInfoFlow { info ->
+    if (url == info.url) {
+      trySend(info)
+    }
+  }
+}
+
+private fun Downloader.Companion.downloadInfoFlow(
+  block: ProducerScope<DownloadInfo>.(DownloadInfo) -> Unit,
+): Flow<DownloadInfo> {
+  return callbackFlow {
+    val callback = object : DownloadInfoCallback {
+      override fun onDownloadInfo(info: DownloadInfo) {
+        block(info)
+      }
+    }
+    FDownloader.registerCallback(callback)
+    awaitClose {
+      FDownloader.unregisterCallback(callback)
+    }
+  }.buffer(Channel.UNLIMITED)
 }
