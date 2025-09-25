def893757123fc62e00d90452ad816281c58200b
zjun <565061763@qq.com>
2025-09-25 10:59:57 +0800

    返回删除的文件个数

--- lib/src/main/java/com/sd/lib/downloader/DownloadDirScope.kt
+++ lib/src/main/java/com/sd/lib/downloader/DownloadDirScope.kt
@@ -1,26 +1,30 @@
 package com.sd.lib.downloader
 
 interface DownloadDirScope {
-  /** 删除子目录下的所有下载文件（不含临时文件） */
-  fun deleteAllDownloadFile()
+  /** 删除子目录下的所有下载文件（不含临时文件），并返回删除的个数 */
+  fun deleteAllDownloadFile(): Int
 
-  /** 删除子目录下的所有临时文件（不含下载中的临时文件） */
-  fun deleteAllTempFile()
+  /** 删除子目录下的所有临时文件（不含下载中的临时文件），并返回删除的个数 */
+  fun deleteAllTempFile(): Int
 }
 
 internal class DownloadDirScopeImpl(
   private val dirname: String,
   private val dir: DownloadDir,
 ) : DownloadDirScope {
-  override fun deleteAllDownloadFile() {
-    dir.files(dirname = dirname) { files ->
-      files.forEach { it.delete() }
+  override fun deleteAllDownloadFile(): Int {
+    return dir.files(dirname = dirname) { files ->
+      var count = 0
+      files.forEach { if (it.delete()) count++ }
+      count
     }
   }
 
-  override fun deleteAllTempFile() {
-    dir.tempFiles(dirname = dirname) { files ->
-      files.forEach { it.delete() }
+  override fun deleteAllTempFile(): Int {
+    return dir.tempFiles(dirname = dirname) { files ->
+      var count = 0
+      files.forEach { if (it.delete()) count++ }
+      count
     }
   }
 }
