e1d6593fdb9f2d663758af59b5b149f4035460c8
zjun <565061763@qq.com>
2025-09-25 10:56:11 +0800

    deleteAllTempFile

--- lib/src/main/java/com/sd/lib/downloader/DownloadDirScope.kt
+++ lib/src/main/java/com/sd/lib/downloader/DownloadDirScope.kt
@@ -3,6 +3,9 @@ package com.sd.lib.downloader
 interface DownloadDirScope {
   /** 删除子目录下的所有下载文件（不含临时文件） */
   fun deleteAllDownloadFile()
+
+  /** 删除子目录下的所有临时文件（不含下载中的临时文件） */
+  fun deleteAllTempFile()
 }
 
 internal class DownloadDirScopeImpl(
@@ -10,7 +13,13 @@ internal class DownloadDirScopeImpl(
   private val dir: DownloadDir,
 ) : DownloadDirScope {
   override fun deleteAllDownloadFile() {
-    return dir.files(dirname = dirname) { files ->
+    dir.files(dirname = dirname) { files ->
+      files.forEach { it.deleteRecursively() }
+    }
+  }
+
+  override fun deleteAllTempFile() {
+    dir.tempFiles(dirname = dirname) { files ->
       files.forEach { it.deleteRecursively() }
     }
   }
